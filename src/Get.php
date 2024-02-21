f21111c5a7a0f983c145e443e89a729092440a03
Guntur <guntur@asi-asiapacific.com>
2024-02-21 12:54:08 +0700

    update check test

--- src/Get.php
+++ src/Get.php
@@ -6,9 +6,23 @@ use GuzzleHttp\Client;
 class Get{
 
     public static function questions(){
-        $client = new Client(['base_uri' => $_ENV['ASI_SURVEY_HOST_ADDR'] ]);
+        /*$client = new Client(['base_uri' => $_ENV['ASI_SURVEY_HOST_ADDR'] ]);
         // Send a request to https://foo.com/api/test
         $response = $client->request('GET', 'test');
         return $response;
+        */
+        $resp = [
+             
+            "project_code"=> "CDP",
+            "project name"=> "'LMS Feedback",
+            "survey_label"=> "Penilaian terhadap materi/ modul pembelajaran",
+            "questions"=> [ 
+                    ["id"=>1,"item"=>"Materi disampaikan secara urut."],
+                    ["id"=>2,"item"=>"Materi pembelajaran mudah dipahami."],
+                    ["id"=>3,"item"=>"Relevansi materi dengan topik pembelajaran."] 
+            ]
+                
+        ];
+            return json_encode($resp); 
     }
 }
