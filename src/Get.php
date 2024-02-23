fb77086112770ed457cf87197976c991a98ac995
ichsan-asi <ichsan@asi-asiapacific.com>
2024-02-23 16:53:43 +0700

    function result

--- src/Get.php
+++ src/Get.php
@@ -39,4 +39,33 @@ class Get{
             'response'  => $data
         ];
     }
+
+    public static function result($survey_code){
+        $client = new Client(['base_uri' => $_ENV['ASI_SURVEY_HOST_ADDR'] ]);
+
+        // Send a request to api
+        $response = $client->request('GET', $survey_code.'/result');
+
+        $data = [];
+        $status = $response->getStatusCode();
+        $message = '';
+
+        if($response->getStatusCode() == 200){
+            try {
+                $contents = $response->getBody()->getContents();
+
+                $data = json_decode($contents, true);
+            } catch (Exception $e) {
+                $message = $e->getMessage();
+
+                SysLogger::save(SysLogger::ERROR, $_SERVER['REQUEST_URI'], 'internal-survey.Get.result', 'Gagal mendapatkan hasil survey, survey_code : '.$survey_code.' - '.$message);
+            }
+        }
+
+        return [
+            'status'    => $status,
+            'message'   => $message,
+            'response'  => $data
+        ];
+    }
 }
